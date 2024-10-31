ca67bc424b9f14d8c48424e79b11c15c7315d115
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-10-31 16:59:36 +0100

    Add mapstruct

--- src/main/java/ua/mibal/web/mapper/ProductMapper.java
+++ src/main/java/ua/mibal/web/mapper/ProductMapper.java
@@ -1,15 +1,20 @@
 package ua.mibal.web.mapper;
 
+import org.mapstruct.Mapper;
 import ua.mibal.domain.Product;
 import ua.mibal.web.dto.ProductDto;
 
 import java.util.List;
 
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
+@Mapper(componentModel = SPRING)
 public interface ProductMapper {
+
     List<ProductDto> toDto(List<Product> products);
 
     ProductDto toDto(Product product);
