533bce789005f83ebec69920e82d2627c8c640a3
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-11-01 09:52:37 +0100

    Extract `ProductRepository` to interface

--- src/main/java/ua/mibal/repository/ProductRepository.java
+++ src/main/java/ua/mibal/repository/ProductRepository.java
@@ -1,13 +1,23 @@
 package ua.mibal.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import ua.mibal.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository {
 
     boolean existsByName(String name);
+
+    Optional<Product> findById(Long id);
+
+    List<Product> findAll();
+
+    Product save(Product entity);
+
+    void deleteById(Long id);
 }
