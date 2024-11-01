b026a357741f59851139d780c7321ed315454646
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-11-01 15:41:22 +0100

    Implement `WebServiceProductRepositoryIntegrationTest` with `WireMock`

---
+++ src/test/java/ua/mibal/repository/web/WireMockTest.java
@@ -0,0 +1,37 @@
+package ua.mibal.repository.web;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public abstract class WireMockTest {
+
+    @RegisterExtension
+    static WireMockExtension wireMockServer = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .configureStaticDsl(true)
+            .build();
+
+    @DynamicPropertySource
+    static void setupTestContainerProperties(DynamicPropertyRegistry registry) {
+        registry.add(
+                "application.rest-client.response-timeout", () -> 1000
+        );
+        registry.add(
+                "application.web-service-product-repository.url",
+                () -> wireMockServer.baseUrl() + "/v1/api/products"
+        );
+        WireMock.configureFor(wireMockServer.getPort());
+    }
+}
