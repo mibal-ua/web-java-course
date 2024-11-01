c9df6baa529d3f6d7558e80feba649dba32c41d8
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-11-01 09:43:25 +0100

    Add Postgres docker test container

--- src/test/java/ua/mibal/test/annotation/UnitTest.java
+++ src/test/java/ua/mibal/test/annotation/UnitTest.java
@@ -1,7 +1,8 @@
 package ua.mibal.test.annotation;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.lang.annotation.Retention;
@@ -13,6 +14,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
 @ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @Retention(RUNTIME)
 public @interface UnitTest {
 }
