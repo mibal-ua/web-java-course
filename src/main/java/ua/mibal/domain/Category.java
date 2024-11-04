971f9746c44674f183bbe5c7ae85233247bed5aa
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-11-04 15:06:55 +0100

    Fix domain

--- src/main/java/ua/mibal/domain/Category.java
+++ src/main/java/ua/mibal/domain/Category.java
@@ -2,28 +2,18 @@ package ua.mibal.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
-//@Entity
-//@Table(name = "category")
 public class Category {
-
-    //    @Id
-//    @GeneratedValue
     private Long id;
-
-    //    @Column(nullable = false, unique = true)
     private String name;
 }
