e9d7ae69646110b3e7b47db23521ff5492c75cc0
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-11-04 15:15:18 +0100

    Completely remove `Spring Data` and test dependencies

--- src/test/java/ua/mibal/ApplicationTests.java
+++ src/test/java/ua/mibal/ApplicationTests.java
@@ -2,9 +2,7 @@ package ua.mibal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import ua.mibal.test.annotation.Postgres;
 
-@Postgres
 @SpringBootTest
 class ApplicationTests {
 
