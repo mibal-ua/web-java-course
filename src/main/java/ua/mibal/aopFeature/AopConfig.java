7376f0014cae25736a735a6ff941b0307f829b87
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-11-14 17:55:30 +0100

    Enable Spring AOP

---
+++ src/main/java/ua/mibal/aopFeature/AopConfig.java
@@ -0,0 +1,13 @@
+package ua.mibal.aopFeature;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+@EnableAspectJAutoProxy
+@Configuration
+public class AopConfig {
+}
---
+++ src/main/java/ua/mibal/aopFeature/FeatureToggleService.java
@@ -0,0 +1,11 @@
+package ua.mibal.aopFeature;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Mykhailo Balakhon
+ * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
+ */
+@Service
+public class FeatureToggleService {
+}
