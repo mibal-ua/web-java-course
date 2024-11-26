f9603e6804623e881b9ee684c1c4b5d870873588
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-11-26 17:49:12 +0100

    Configure mappers by TestConfiguration

--- src/test/java/ua/mibal/web/ControllerTest.java
+++ src/test/java/ua/mibal/web/ControllerTest.java
@@ -1,14 +1,15 @@
 package ua.mibal.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import ua.mibal.web.config.MapperConfig;
 
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
-@ComponentScan("ua.mibal.web.mapper")
+@Import(MapperConfig.class)
 public abstract class ControllerTest {
 
     @Autowired
