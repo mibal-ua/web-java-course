052983c9d24f20395a21b0f5497ab0be391919bd
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-11-29 22:00:27 +0100

    Fix `WireMockTest`

--- src/test/java/ua/mibal/service/storage/WireMockTest.java
+++ src/test/java/ua/mibal/service/storage/WireMockTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import ua.mibal.test.annotation.DatabaseTest;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -14,6 +15,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
+@DatabaseTest
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public abstract class WireMockTest {
 
