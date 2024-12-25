a50dced651c1883fa33ed5255ca37f4a1e3bfae3
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-12-25 10:38:04 +0100

    Move method-based auth annotation to controller

--- src/main/java/ua/mibal/web/CosmoCatController.java
+++ src/main/java/ua/mibal/web/CosmoCatController.java
@@ -1,6 +1,7 @@
 package ua.mibal.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,8 @@ import java.util.List;
 public class CosmoCatController {
     private final CosmoCatService service;
     private final CosmoCatMapper mapper;
-    
+
+    @PreAuthorize("hasRole('COSMO_ADMIN')")
     @GetMapping
     public List<CosmoCatDto> getCosmoCats() {
         return mapper.toDto(
